66221316e54d5be1a85e99ad394ff07df9d00ff2
Denis Klester <dinisoft@gmail.com>
2020-02-07 15:59:31 +0600

    add event

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,11 @@
 (function() {
     // Theme switch
-    //var theme
+    const toggleTheme = document.querySelector("#toggle-theme");
+    toggleTheme.addEventListener("click", function () {
+        if (document.documentElement.hasAttribute("theme")) {
+            document.documentElement.removeAttribute("theme");
+        } else {
+            document.documentElement.setAttribute("theme", "special");
+        }
+    });
 }());
